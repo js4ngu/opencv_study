d17943e46ba5d4a695e2d5518442836fe0345557
Jongsang Yoo <jamin0513@gmail.com>
2020-12-19 12:31:56 +0900

    imwrite!

--- ch01/hellocv.py
+++ ch01/hellocv.py
@@ -3,13 +3,15 @@ import sys
 
 print('Hello openCV', cv2.__version__)
 
-img = cv2.imread('D:\FIles\study\open-cv\study\opencv_study\ch01\img.bmp')
+img = cv2.imread('D:\FIles\study\open-cv\study\opencv_study\ch01\img.bmp', cv2.IMREAD_GRAYSCALE)
 
 if img is None:
     print('Image load failed')
     sys.exit()
 
-cv2.namedWindow('image')
+cv2.imwrite('D:\FIles\study\open-cv\study\opencv_study\ch01\cat_gray2.png', img)
+
+cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
 cv2.imshow('image', img)
 cv2.waitKey(0)
 
